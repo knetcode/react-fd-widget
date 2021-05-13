33abd33a8edf0a4b331a64077611c7ccc12de65c
Kyle Esterhuizen <knetcode@gmail.com>
2021-05-13 16:58:28 +0200

    slight auth

--- src/Components/NoKey.js
+++ src/Components/NoKey.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import { TextInput } from 'react-materialize'
 
-const NoKey = ({ API_KEY, setAPI_KEY }) => {
+const NoKey = ({ setAPI_KEY }) => {
 	const apiCapture = (e) => {
 		e.preventDefault()
-		const api_input = document.querySelector('#api-input')
-		const api_formatted = `Basic ${window.btoa(api_input.value.trim())}=`
+		const api_input = document.querySelector('#api-input').value
+		const api_formatted = `Basic ${window.btoa(api_input)}=`
 		setAPI_KEY(api_formatted)
 		localStorage.setItem('API_KEY', JSON.stringify(api_formatted))
-		console.log(API_KEY)
 	}
 
 	return (
 		<div className='no-key'>
 			<form className='no-key-form' onSubmit={(e) => apiCapture(e)}>
-				<TextInput id='api_input' label='Enter your API Key' password />
+				<TextInput id='api-input' label='Enter your API Key' />
 				<button type='submit' className='btn ctk-red btn-block waves-effect waves-light'>
 					Submit
 				</button>
